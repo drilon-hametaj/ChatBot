5584a9f90d79dae8e1e82c396e9514c814f1a0df
DrilonHametaj <48368584+drilon-hametaj@users.noreply.github.com>
2020-07-11 18:43:29 +0200

    ChatBot

---
+++ main.py
@@ -0,0 +1,123 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
+try:
+    #if model exist
+    with open("data.pickle", "rw") as f:
+        words, labels, training, output = pickle.load(f)
+
+
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern) #find similar words
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+
+            if intent["tag"] not in (labels):
+                labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    # Training now
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training) #For machine learning we need to convert into numpy array
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+
+#Model
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])]) #input data
+net = tflearn.fully_connected(net, 8) #8 neurons
+net = tflearn.fully_connected(net, 8) #8 neurons
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax") #probability for each output
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    #if model exist
+    model.load("model.tflearn")
+except:
+    #Fit
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+#Prediction
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+def chat():
+    print("Start talking with me (quit for stopping)")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)]) #return a probability
+        results_index = numpy.argmax(results) #return index greater value in list of pribability
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                response = tg['responses']
+
+        print(random.choice(response))
+
+
+chat()
